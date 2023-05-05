9328a4d424ea70b291b062fdb2db9cd753e59ae0
Rajesh <rajeshdyawarkonda123@gmail.com>
2023-05-06 03:58:05 +0530

    server project

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 function loggerMiddleware(req, res, next) {
 	const file = "logs.txt";
 	const log = `${req.method} ${Date()}\n`;
+	console.log(log)
 	fs.appendFileSync(file, log);
 	next();
 }
